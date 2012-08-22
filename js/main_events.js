fc399c0ecd14af8fefb05fadabbbd409e90fc649
Feifei HANG <l.daniel.hung@gmail.com>
2012-08-22 18:03:02 +0100

    added dashboard; jQuery library; Kinetic library

---
+++ js/main_events.js
@@ -0,0 +1,8 @@
+function visibleElement(elementId) {
+    document.getElementById(elementId).style.visibility = 'visible';
+}
+
+function invisibleElement(elementId) {
+    document.getElementById(elementId).style.visibility = 'hidden';
+}
+
