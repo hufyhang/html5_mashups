0b09bdb90aca064e76c7f36c0471be466c44e230
Feifei HANG <l.daniel.hung@gmail.com>
2012-08-24 00:47:44 +0100

    improved adding feeds form

--- js/main_events.js
+++ js/main_events.js
@@ -40,7 +40,7 @@ function showFeedsPanel(containerId) {
 function showAddFeedForm(containerId) {
     visibleElement('dashboard');
     visibleElement('dashboard_div');
-    document.getElementById(containerId).innerHTML = '<table class="frame_table" cellpadding="5px"><tr><td><label>Feed URL: </label><input type="TEXT" id="add_feed_form_feed_url" class="input_box"/></td></tr><tr><td><label>Name: </label><input type="TEXT" id="add_feed_form_feed_name" class="input_box" required="required"/></td></tr><tr><td><label>Type: </label><br /><input type="RADIO" id="add_feed_form_feed_type_rest" name="type" style="margin-left: 50px;" value="REST/HTTP" checked="true">REST/HTTP</input><br /><input type="RADIO" id="add_feed_form_feed_type_soap" name="type" style="margin-left: 50px;" value="SOAP">SOAP</input></td></tr><tr><td><div class="div_push_button" onclick="addFeedFromFeedForm(\''+ containerId + '\')">OK</div><div class="div_push_button" onclick="cancelAddNewFeed(\'' + containerId + '\')">Cancel</div></td></tr></table>';
+    document.getElementById(containerId).innerHTML = '<table class="frame_table" cellpadding="5px"><tr><td><label>Feed URL: </label><input type="TEXT" id="add_feed_form_feed_url" class="input_box"/></td></tr><tr><td><label>Name: </label><input type="TEXT" id="add_feed_form_feed_name" class="input_box" required="required"/></td></tr><tr><td><label>Type: </label><br /><input type="RADIO" id="add_feed_form_feed_type_rest" name="type" style="margin-left: 50px;" value="REST/HTTP" checked="true">REST/HTTP</input><br /><input type="RADIO" id="add_feed_form_feed_type_soap" name="type" style="margin-left: 50px;" value="SOAP">SOAP</input></td></tr><tr><td><div class="div_push_button" onclick="addFeedFromFeedForm(\''+ containerId + '\')">Add</div><div class="div_push_button" onclick="closeAddNewFeed(\'' + containerId + '\')">Close</div></td></tr></table>';
 }
 
 function addFeedFromFeedForm(containerId) {
@@ -75,16 +75,16 @@ function addFeedFromFeedForm(containerId) {
     _database.transaction(function(tx) {
         tx.executeSql('INSERT INTO feeds (name, url, feed_type) VALUES (\"' + name + '\", \"' + url +'\", \"' + type + '\")');
 
-    updateFeedsHTML();
-    // close form
-    cancelAddNewFeed(containerId);
-    showFeedsPanel(_current_container_id);
+        document.getElementById('add_feed_form_feed_url').value = document.getElementById('add_feed_form_feed_name').value = '';
+        alert("Feed added.");
+        updateFeedsHTML();
     });
 }
 
-function cancelAddNewFeed(containerId) {
+function closeAddNewFeed(containerId) {
     invisibleElement('dashboard_div');
     invisibleElement('dashboard');
+    showFeedsPanel(_current_container_id);
 }
 
 function appendFeedsNameList(name) {
