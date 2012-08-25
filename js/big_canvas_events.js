ddd79667aec370912cb40fbb3d32f3781849787c
Feifei HANG <l.daniel.hung@gmail.com>
2012-08-25 20:10:33 +0100

    feeds connectorss

--- js/big_canvas_events.js
+++ js/big_canvas_events.js
@@ -12,6 +12,8 @@ function initialiseBigCanvas() {
 
     _big_canvas_layer = new Kinetic.Layer();
     _big_canvas_stage.add(_big_canvas_layer);
+
+    drawStartAndEndNodes();
 }
 
 function resizeBigCanvas() {
@@ -20,6 +22,135 @@ function resizeBigCanvas() {
     // var height = canvas.offsetHeight;
     // _big_canvas_stage.setSize(width, height);
     // _big_canvas_stage.draw();
+}
+
+function moveConnector(connector, parent_node) {
+    connector.setPosition(parent_node.getX() + parent_node.getWidth() - parent_node.getWidth()/2, 
+                            parent_node.getY() + parent_node.getBoxHeight());
+    _big_canvas_stage.draw();
+}
+
+function createConnector(parent_node) {
+    // connector
+    var connector = new Kinetic.Circle({
+            x: parent_node.getX() + parent_node.getWidth() - parent_node.getWidth()/2,
+            y: parent_node.getY() + parent_node.getBoxHeight(),
+            radius: 8,
+            fill: '#00A8E6',
+            stroke: 'black',
+            strokeWidth: 1
+    });
+    connector.on("mouseover", function() {
+        this.setFill('#66A8FF');
+        this.setStroke('orange');
+        this.setStrokeWidth(3);
+        _big_canvas_layer.draw();
+    });
+    connector.on("mouseout", function() {
+        this.setFill('#00A8E6');
+        this.setStroke('black');
+        this.setStrokeWidth(1);
+        _big_canvas_layer.draw();
+    });
+    return connector;
+}
+
+function drawStartAndEndNodes() {
+    var start = new Kinetic.Text({
+            draggable: true,
+            x:  _big_canvas_stage.getWidth() / 2,
+            y: 10,
+            stroke: '#555',
+            strokeWidth: 2,
+            fill: '#aaa',
+            text: 'Start',
+            fontSize: 15,
+            fontFamily: 'Calibri',
+            textFill: 'white',
+            width: 80,
+            padding: 10,
+            align: 'center',
+            // fontStyle: 'italic',
+            shadow: {
+                color: 'black',
+                blur: 1,
+                offset: [5, 5],
+                opacity: 0.2
+            },
+            cornerRadius: 15
+    });
+    var startConnector = createConnector(start);
+
+    start.on('mouseover', function() {
+        this.setStroke('red');
+        _big_canvas_layer.draw();
+    });
+    start.on('mouseout', function() {
+        this.setStroke('#ddd');
+        _big_canvas_layer.draw();
+    });
+
+    start.on("dragstart", function() {
+        feed.moveToTop();
+        _big_canvas_layer.draw();
+    });
+
+    start.on("dragmove", function(){
+        moveConnector(startConnector, start);
+    });
+
+    var end = new Kinetic.Text({
+            draggable: true,
+            x: _big_canvas_stage.getWidth() / 2,
+            y: _big_canvas_stage.getHeight() - 60,
+            stroke: '#555',
+            strokeWidth: 2,
+            fill: '#aaa',
+            text: 'End',
+            fontSize: 15,
+            fontFamily: 'Calibri',
+            textFill: 'white',
+            width: 80,
+            padding: 10,
+            align: 'center',
+            // fontStyle: 'italic',
+            shadow: {
+                color: 'black',
+                blur: 1,
+                offset: [5, 5],
+                opacity: 0.2
+            },
+            cornerRadius: 15
+    });
+
+    // var endConnector = createConnector(end);
+
+    end.on('mouseover', function() {
+        this.setStroke('red');
+        _big_canvas_layer.draw();
+    });
+    end.on('mouseout', function() {
+        this.setStroke('#ddd');
+        _big_canvas_layer.draw();
+    });
+
+    end.on("dragstart", function() {
+        feed.moveToTop();
+        _big_canvas_layer.draw();
+    });
+
+    // end.on("dragmove", function() {
+    //     moveConnector(endConnector, end);
+    // });
+
+
+    _big_canvas_layer.add(start);
+    _big_canvas_layer.add(end);
+
+    _big_canvas_layer.add(startConnector);
+    // _big_canvas_layer.add(endConnector);
+
+    _big_canvas_stage.draw();
 }
 
 function updateLocalStogate() {
@@ -39,13 +170,13 @@ function drawARestFeed(name, url) {
             draggable: true,
             x: 0,
             y: 0,
-            stroke: '#555',
-            strokeWidth: 2,
-            fill: '#ddd',
+            stroke: 'black',
+            strokeWidth: 1,
+            fill: '#ddf',
             text: name + '\n\n' + url,
             fontSize: 10,
             fontFamily: 'Calibri',
-            textFill: '#555',
+            textFill: 'black',
             width: 350,
             padding: 10,
             align: 'center',
@@ -59,12 +190,14 @@ function drawARestFeed(name, url) {
             cornerRadius: 5
     });
 
+    var feedConnector = createConnector(feed);
+
     feed.on('mouseover', function() {
         this.setStroke('red');
         _big_canvas_layer.draw();
     });
     feed.on('mouseout', function() {
-        this.setStroke('#ddd');
+        this.setStroke('black');
         _big_canvas_layer.draw();
     });
 
@@ -74,13 +207,18 @@ function drawARestFeed(name, url) {
 
     feed.on("dragstart", function() {
         feed.moveToTop();
+        feedConnector.moveToTop();
         _big_canvas_layer.draw();
+    });
+
+    feed.on("dragmove", function() {
+        moveConnector(feedConnector, feed);
     });
 
     _feeds_nodes.push(feed);
 
     _big_canvas_layer.add(feed);
-    _big_canvas_stage.add(_big_canvas_layer);
+    _big_canvas_layer.add(feedConnector);
     _big_canvas_stage.draw();
 } 
 
