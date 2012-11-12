8a7028acf5c59809c78086cbac152697fd114836
Feifei HANG <l.daniel.hung@gmail.com>
2012-11-12 11:10:39 +0000

    safe-mode-friendly Access Checking module
    safe-mode-friendly Access Checking module

--- php/phpCheckAccess.php
+++ php/phpCheckAccess.php
@@ -1,8 +1,16 @@
 <?php
 $url = str_replace(' ','%20',$_GET['url']);
-echo get_http_response_code($url);
-function get_http_response_code($url) {
-    $headers = get_headers($url);
-    return substr($headers[0], 9, 3);
-}
+// echo get_http_response_code($url);
+// function get_http_response_code($url) {
+//     $headers = get_headers($url);
+//     return substr($headers[0], 9, 3);
+// }
+$curl = curl_init();
+curl_setopt_array( $curl, array(
+    CURLOPT_RETURNTRANSFER => true,
+    CURLOPT_URL => $url) );
+curl_exec( $curl );
+$response_code = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
+curl_close( $curl );
+echo $response_code;
 ?>
