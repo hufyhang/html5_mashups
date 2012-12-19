4c7738e88525ca614eb8a2666d92aa4226548608
Feifei HANG <l.daniel.hung@gmail.com>
2012-12-19 22:17:41 +0000

    fully support HATEOAS

--- php/phpGet.php
+++ php/phpGet.php
@@ -20,6 +20,7 @@ function url_get_contents ($url) {
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $url);
     curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
     // curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 2); 
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     $output = curl_exec($ch);
