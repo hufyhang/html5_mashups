698be1ed0810c4f53972273eed948c46d9e07450
Feifei HANG <l.daniel.hung@gmail.com>
2012-12-19 15:20:23 +0000

    quick commit

--- php/phpCheckAccess.php
+++ php/phpCheckAccess.php
@@ -15,12 +15,18 @@ curl_setopt_array( $curl, array(
     CURLOPT_RETURNTRANSFER => true,
     CURLOPT_URL => $url) );
 // curl_setopt ($curl, CURLOPT_CAINFO, dirname(__FILE__)."/cacert.pem");
+curl_setopt($curl, CURLOPT_VERBOSE, true);
 curl_setopt($curl, CURLOPT_FOLLOWLOCATION, 1);
 curl_setopt($curl, CURLOPT_HTTPAUTH, CURLAUTH_ANY);
 curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
 // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, 2); 
-curl_exec( $curl );
+$res = curl_exec( $curl );
 $response_code = curl_getinfo( $curl, CURLINFO_HTTP_CODE );
+// print "<pre>\n";
+// print_r(curl_getinfo($curl));
+// echo "\n\ncURL error number:" .curl_errno($curl); // print error info
+// echo "\n\ncURL error:" . curl_error($curl); 
+// print "</pre>\n";
 curl_close( $curl );
 echo $response_code;
 ?>
